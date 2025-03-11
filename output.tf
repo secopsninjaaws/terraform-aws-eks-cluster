bf5cf1e15c2b895ee9862d7fa9f1ebef05c248c2
secopsninjaaws <lucas@secopsninjaaws.com.br>
2025-03-11 11:33:28 -0300

    fmt

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "oidc" {
-    value = data.tls_certificate.eks_oidc_tls_certificate.certificates[*].sha1_fingerprint
-  
+  value = data.tls_certificate.eks_oidc_tls_certificate.certificates[*].sha1_fingerprint
+
 }
