b6c5fd62a7e06e98f98ed347d863b76f1d4cf770
secopsninjaaws <lucas@secopsninjaaws.com.br>
2025-03-11 17:21:08 -0300

    f

--- karpenter-helm.tf
+++ karpenter-helm.tf
@@ -6,45 +6,45 @@ provider "helm" {
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.main.id]
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.main.id, "--region", var.region]
     }
   }
 }
 
 resource "helm_release" "karpenter" {
-  name             = "karpenter"
-  repository       = "https://charts.karpenter.sh/"
-  chart            = "karpenter"
-  version          = var.chart_version
-  namespace        = var.karpenter_namespace
-  create_namespace = true
-  depends_on = [
-    aws_eks_cluster.main,
-    aws_eks_node_group.main,
-    aws_iam_role.karpenter_controller,
-    aws_iam_role_policy_attachment.karpenter_controller_policy_attachment,
-    aws_iam_policy.karpenter_controller,
-    aws_iam_policy_attachment.karpenter_controller_policy_attachment,
-    aws_iam_instance_profile.karpenter
-  ]
-  set {
-    name  = "serviceAccount.annotations.eks.amazonaws.com/role-arn"
-    value = aws_iam_role.karpenter_controller.arn
-  }
+    name       = "karpenter"
+    repository = "https://charts.karpenter.sh/"
+    chart      = "karpenter"
+    version    = var.chart_version
+    namespace  = var.karpenter_namespace
+    create_namespace = true
+    depends_on = [
+        aws_eks_cluster.main,
+        aws_eks_node_group.main,
+        aws_iam_role.karpenter_controller,
+        aws_iam_role_policy_attachment.karpenter_controller_policy_attachment,
+        aws_iam_policy.karpenter_controller,
+        aws_iam_policy_attachment.karpenter_controller_policy_attachment,
+        aws_iam_instance_profile.karpenter
+    ]
+    set {
+        name  = "serviceAccount.annotations.eks.amazonaws.com/role-arn"
+        value = aws_iam_role.karpenter_controller.arn
+    }
 
-  set {
-    name  = "clusterName"
-    value = aws_eks_cluster.main.id
-  }
+    set {
+        name  = "clusterName"
+        value = aws_eks_cluster.main.id
+    }
 
-  set {
-    name  = "clusterEndpoint"
-    value = aws_eks_cluster.main.endpoint
-  }
+    set {
+        name  = "clusterEndpoint"
+        value = aws_eks_cluster.main.endpoint
+    }
 
-  set {
-    name  = "aws.defaultInstanceProfile"
-    value = aws_iam_instance_profile.karpenter.name
-  }
+    set {
+      name = "aws.defaultInstanceProfile"
+      value = aws_iam_instance_profile.karpenter.name
+    }
 }
 
