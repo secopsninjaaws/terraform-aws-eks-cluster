370a6e286915af20aa14f9b3235a0a91d7bd4c02
secopsninjaaws <lucas@secopsninjaaws.com.br>
2025-03-11 15:40:12 -0300

    fix:ca

--- karpenter-helm.tf
+++ karpenter-helm.tf
@@ -1,7 +1,7 @@
 provider "helm" {
   kubernetes {
     host                   = aws_eks_cluster.main.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certicate_authority.0.data)
+    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority.0.data)
 
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
