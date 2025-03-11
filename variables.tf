4a21a424b66d15b7ac88e968fdefbe3f654cc6e1
secopsninjaaws <lucas@secopsninjaaws.com.br>
2025-03-11 13:02:18 -0300

    fix: variables

--- variables.tf
+++ variables.tf
@@ -12,25 +12,6 @@ variable "project_name" {
   default     = "Lucas-EKS-Module"
 
 }
-
-variable "public_subnet_1a" {
-  description = "The public subnet in the first availability zone"
-  type        = string
-
-}
-
-variable "public_subnet_1b" {
-  description = "The public subnet in the second availability zone"
-  type        = string
-
-}
-
-variable "public_subnet_1c" {
-  description = "The public subnet in the third availability zone"
-  type        = string
-
-}
-
 variable "private_subnet_1a" {
   description = "The private subnet in the first availability zone"
   type        = string
