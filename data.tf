ac598e383cdadd88518d9648d4ecb4bffde7ab46
secopsninjaaws <lucas@secopsninjaaws.com.br>
2025-03-11 17:35:50 -0300

    t

--- data.tf
+++ data.tf
@@ -1,7 +1,3 @@
 data "tls_certificate" "eks_oidc_tls_certificate" {
   url = aws_eks_cluster.main.identity[0].oidc[0].issuer
 }
-
-data "aws_eks_cluster_auth" "eks" {
-  name = aws_eks_cluster.main.id
-}
--- karpenter-helm.tf
+++ karpenter-helm.tf
@@ -1,8 +1,8 @@
 provider "helm" {
   kubernetes {
     host                   = aws_eks_cluster.main.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks.token
+    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+    token                  = aws_eks_cluster_auth.main.token
 
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
