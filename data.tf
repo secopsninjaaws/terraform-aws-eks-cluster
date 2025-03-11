f79d660f083a988415eab3ddbd88fe86d9d6b631
secopsninjaaws <lucas@secopsninjaaws.com.br>
2025-03-11 17:27:54 -0300

    t

---
+++ data.tf
@@ -0,0 +1,4 @@
+data "tls_certificate" "eks_oidc_tls_certificate" {
+  url = aws_eks_cluster.main.identity[0].oidc[0].issuer
+
+}
--- oidc.tf
+++ oidc.tf
@@ -1,8 +1,3 @@
-data "tls_certificate" "eks_oidc_tls_certificate" {
-  url = aws_eks_cluster.main.identity[0].oidc[0].issuer
-
-}
-
 resource "aws_iam_openid_connect_provider" "main" {
   url             = aws_eks_cluster.main.identity[0].oidc[0].issuer
   client_id_list  = ["sts.amazonaws.com"]
